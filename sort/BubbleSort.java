ccc20494ca3ad9f89a8b54e3e7c8521460d65db4
Sairam Chekuri <sairamc3@gmail.com>
2023-11-09 10:58:26 +0530

    Formatting and adding comments

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -7,37 +7,42 @@ class BinaryRecursiveSearch {
 
 		int[] arr = { 500, 62, 71, 22, 2, 3, 4, 10, 400 };
 
-		 String givenArrayInString = Arrays.stream(arr)
-                                .mapToObj(String::valueOf)
-                                .collect(Collectors.joining(", ", "{", "}"));
+		String givenArrayInString = Arrays.stream(arr)
+				.mapToObj(String::valueOf)
+				.collect(Collectors.joining(", ", "{", "}"));
 		System.out.println(givenArrayInString);
-		
+
 		bubbleSort(arr);
-		
+
 		System.out.println("After Sorting");
-		
+
 		String afterSorting = Arrays.stream(arr)
 				.mapToObj(String::valueOf)
 				.collect(Collectors.joining(", ", "{", "}"));
-		System.out.println(afterSorting); 
-		
-
+		System.out.println(afterSorting);
 
 	}
 
-	private static void bubbleSort(int[] arr){
-		
-		for(int i=0; i<arr.length; i++){
-			for(int j=0; j<arr.length-1;j++){
-				if(arr[j+1]<arr[j]){
+	/**
+	 * Complexity O(n^2)
+	 * Space Complexity O(1)
+	 * 
+	 * We are not creating any new data structure and hence space complexity is less
+	 * But for every case the computing complexity is O(n^2), which is not advised
+	 * @param arr
+	 */
+	private static void bubbleSort(int[] arr) {
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - 1; j++) {
+				if (arr[j + 1] < arr[j]) {
 
 					int temp = arr[j];
-					arr[j]=arr[j+1];
-					arr[j+1]=temp;
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
 				}
 			}
-		}	
-
+		}
 
 	}
 
