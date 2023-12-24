826da5cd5d8aeb61dc323d0172c53cf08f48ac00
Sairam Chekuri <sairamc3@gmail.com>
2023-12-24 16:43:24 +0530

    Just typed my approach and need to still implment the solution

---
+++ hackerearth/algorithms/FindMex/FindMex.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.*;
+
+
+class FindMex {
+
+
+    public static void main(String args[] ) throws Exception {
+        
+
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int noOfCases =Integer.parseInt(br.readLine()) ; 
+        
+        
+       
+        
+
+    }
+}
