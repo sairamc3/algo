a0e35899bfbaa0f228c652dcfc3160bdc1430cb0
Sairam Chekuri <sairamc3@gmail.com>
2023-12-25 20:58:00 +0530

    Optimum Solution has acheived

--- hackerearth/algorithms/FindMex/FindMexOptimized.java
+++ hackerearth/algorithms/FindMex/FindMexOptimized.java
@@ -5,123 +5,109 @@ import java.io.InputStreamReader;
 import java.util.*;
 import java.util.stream.*;
 
-
 class FindMex {
 
     private static Map<Integer, Integer> count = new HashMap<>();
 
+    public static void main(String args[]) throws Exception {
 
-    public static void main(String args[] ) throws Exception {
-        
-
-        //BufferedReader
+        // BufferedReader
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int sizeOfArray =Integer.parseInt(br.readLine()) ; 
+        int sizeOfArray = Integer.parseInt(br.readLine());
 
         String arrayAsString = br.readLine();
 
         String[] inputAsString = arrayAsString.split(" ");
 
-        List<Integer> inputList=Arrays.stream(inputAsString)
-                                        .map(Integer::parseInt)
-                                        .collect(Collectors.toList());
-    
+        List<Integer> inputList = Arrays.stream(inputAsString)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
         addTheListToMap(inputList);
 
-       
+        // This method is a valid solution
+        // But this is brute force approach and it is taking more time to execute.
+        // So need to change the strategy
+        // for(int i=0; i < inputList.size();i++){
 
-       // This method is a valid solution
-       // But this is brute force approach and it is taking more time to execute.
-       // So need to change the strategy 
-       //for(int i=0; i < inputList.size();i++){
+        // Integer mex = findMex(inputList.subList(0,i+1));
+        // System.out.print(mex + " ");
+        // }
 
-            //Integer mex = findMex(inputList.subList(0,i+1));
-            //System.out.print(mex + " ");
-       //}
-
-       // Another approach
-       // We need to do it only once, saves time. for the full list. 
+        // Another approach
+        // We need to do it only once, saves time. for the full list.
 
         // The below solution did not work, need to find out why.
-       Integer mexForWhole=findMex(inputList);
+        Integer mexForWhole = findMex(inputList);
 
-       List<Integer> resultList = new ArrayList<>(sizeOfArray);
-       resultList.add(mexForWhole);
-
-       
-
-       for(int i=sizeOfArray-1; i > 0; i--){
-
-        if(mexForWhole > i ){
-
-            if(count.containsKey(mexForWhole) && count.get(mexForWhole) > 0){
-                 Integer noOfInstances=count.get(mexForWhole);
-                count.remove(mexForWhole);
-                count.put(mexForWhole, noOfInstances-1);
-                continue;
-            }
-
-            mexForWhole= inputList.get(i);
-
-            
-        } 
+        List<Integer> resultList = new ArrayList<>(sizeOfArray);
         resultList.add(mexForWhole);
 
-       }
+        for (int i = sizeOfArray - 1; i > 0; i--) {
 
-       
-       // Print the reusult which should be in reverse order
-       for(int result= resultList.size()-1; result>=0; result--){
+            if (mexForWhole > inputList.get(i)) {
 
-        System.out.print(resultList.get(result) + " ");
-       }
+                if (count.containsKey(inputList.get(i))) {
 
-        
+                    if (count.get(inputList.get(i)) > 1) {
+                        Integer noOfInstances = count.get(inputList.get(i));
+                        count.remove(inputList.get(i));
+                        count.put(inputList.get(i), noOfInstances - 1);
+                    } else {
+                        mexForWhole = inputList.get(i);
+                    }
+
+                }
+            }
+            resultList.add(mexForWhole);
+        }
+        // Print the reusult which should be in reverse order
+        for (int result = resultList.size() - 1; result >= 0; result--) {
+
+            System.out.print(resultList.get(result) + " ");
+        }
 
     }
 
-    private static void addTheListToMap(List<Integer> inputList){
+    private static void addTheListToMap(List<Integer> inputList) {
 
-        for(Integer given:inputList){
+        for (Integer given : inputList) {
 
-            if(count.containsKey(given)){
-                Integer noOfInstances=count.get(given);
+            if (count.containsKey(given)) {
+                Integer noOfInstances = count.get(given);
                 count.remove(given);
-                count.put(given, noOfInstances+1);
+                count.put(given, noOfInstances + 1);
 
             } else {
                 count.put(given, 1);
             }
-            
+
         }
     }
 
+    private static Integer findMex(List<Integer> inputList) {
 
-
-    private static Integer findMex(List<Integer> inputList){
-    
         Integer mex = 0;
 
-        Set<Integer> unsortedSet =inputList.stream().collect(Collectors.toSet());
+        Set<Integer> unsortedSet = inputList.stream().collect(Collectors.toSet());
 
         List<Integer> distinctList = unsortedSet.stream().collect(Collectors.toList());
 
-        if(distinctList.get(0) != 0) {
-            
+        if (distinctList.get(0) != 0) {
+
             return mex;
         }
 
-        for(int i=0; i<distinctList.size()-1; i++){
-        
-            if(distinctList.get(i+1)- distinctList.get(i) != 1){
+        for (int i = 0; i < distinctList.size() - 1; i++) {
 
-                mex = distinctList.get(i)+1;
+            if (distinctList.get(i + 1) - distinctList.get(i) != 1) {
+
+                mex = distinctList.get(i) + 1;
                 return mex;
             }
         }
 
-        
-        mex = distinctList.get(distinctList.size()-1)+1;
+        mex = distinctList.get(distinctList.size() - 1) + 1;
 
         return mex;
     }
